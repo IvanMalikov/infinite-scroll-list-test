179ca1d0ca57510ff83f447478d6c42ff599a83c
IvanMalikov <vanya.230715@gmail.com>
2024-12-17 12:21:15 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   target: 'static',
+  ssr: false,
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt'
   ],
-  plugins: ["~/plugins/vue-query.ts"]
+  plugins: ["~/plugins/vue-query.ts"],
+  api: {
+    baseURL: '/infinite-scroll-list/'
+  }
 })
