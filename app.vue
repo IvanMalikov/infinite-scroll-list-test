62770bd6a61015f1a5cd15a4d176fd615cc56f4a
IvanMalikov <vanya.230715@gmail.com>
2024-12-17 09:30:54 +0100

    leftovers

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <InfiniteList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import InfiniteList from '~/components/InfiniteList.vue'
+</script>
